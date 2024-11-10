115a5e5862d649a5e679d46c98050a6a9be8ef91
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-11-10 20:56:56 +0000

    wrote tests of user model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 from django.contrib.auth.models import (
     AbstractBaseUser, BaseUserManager, PermissionsMixin
 )
@@ -9,20 +9,42 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return user"""
+        email = self.normalize_email(email)
+
+        if not email:
+            raise ValueError("Please provide email address")
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db) #for saving into multiple dbs
 
         return user
 
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and return super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 class User(AbstractBaseUser, PermissionsMixin):
     """user in the system """
     email = models.EmailField(max_length=256, unique=True)
-    name = models.CharField(max_length=256)
+    first_name = models.CharField(max_length=256, null=True, blank=True)
+    last_name = models.CharField(max_length=256, null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    phone = models.CharField(max_length=15, blank=True)
+    bio = models.TextField(blank=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
 
     # connects to usermanager class
     objects = UserManager()
     # to use email as username
     USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.name
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+class UserTest(TestCase):
+    """test cases for users"""
 
-# Create your tests here.
+    def test_create_superuser(self):
+        """Tests creating a superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+    def test_create_user(self):
+        """test for creating user"""
+        user = get_user_model().objects.create_user(
+            email="test@example.com",
+            password='test123'
+        )
+
+        self.assertEqual(user.email,'test@example.com')
+        # self.assertEqual(user.password, 'test123')
+
+    def test_create_user_without_email(self):
+        """no email provided test"""
+
+        with self.assertRaises(ValueError) as e:
+            user = get_user_model().objects.create_user(
+                email='',
+                password='test123'
+            )
+
