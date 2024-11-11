c71004dd8e229a102cc4e477ef5f127b31da8cc0
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-11-11 19:49:41 +0000

    installed drf spectacular

--- TagAlong/settings.py
+++ TagAlong/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'restframework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMWORK = {
+    "DEFAULT_SCHEMA_CLASS":'drf_spectacular.openapi.AutoSchema',
+}
